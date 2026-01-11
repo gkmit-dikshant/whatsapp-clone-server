53c1ffa60f134d0382b3f201ecf0e097fc059ad3
gkmit-dikshant <dikshant@gkmit.co>
2026-01-11 09:56:00 +0530

    feat: add chats api, services and controllers

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ChatsService } from './chats.service';
+import { ChatsController } from './chats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatUser } from './entities/chat-user.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Chat } from './entities/chat.entity';
 
 @Module({
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Chat, ChatUser, User])],
+  controllers: [ChatsController],
+  providers: [ChatsService],
 })
 export class ChatsModule {}
--- src/chats/entities/chat-user.entity.ts
+++ src/chats/entities/chat-user.entity.ts
@@ -4,13 +4,14 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Chat } from './chat.entity';
 
-@Entity('chat_members')
+@Entity('chat_users')
 export class ChatUser {
   @PrimaryGeneratedColumn({ type: 'int' })
   id: number;
@@ -18,13 +19,18 @@ export class ChatUser {
   @Column({ name: 'chat_id', type: 'int' })
   chatId: number;
 
+  @Column({ name: 'user_id', type: 'int' })
+  userId: number;
+
   @Column({ name: 'is_admin', default: false })
   isAdmin: boolean;
 
   @ManyToOne(() => User, (user) => user.chatUsers)
+  @JoinColumn({ name: 'user_id' })
   user: User;
 
   @ManyToOne(() => Chat, (chat) => chat.chatUsers)
+  @JoinColumn({ name: 'chat_id' })
   chat: Chat;
 
   @CreateDateColumn({ name: 'created_at' })
